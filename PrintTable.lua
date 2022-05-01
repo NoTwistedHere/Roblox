8d7edcf633581ee256093fc7feba2e8dc22a545a
NoTwistedHere <notwistedhere@gmail.com>
2022-05-01 12:11:42 +0100

    Final Fix

--- PrintTable.lua
+++ PrintTable.lua
@@ -105,7 +105,7 @@ local function ParseObject(Object, DetailedInfo, TypeOf)
             return Tostring(Object), getrawmetatable(Object) and " [Metatable]"
         elseif ObjectType == 5 then
             local Info = getinfo(Object)
-            return ("%s"):format(tostring(Object)), ("source: %s, what: %s, name: %s (currentline: %s, numparams: %s, nups: %s, is_vararg: %s)"):format(Stringify(Info.source), Info.what, Stringify(Info.name), Info.currentline, Info.numparams, Info.nups, Info.is_vararg)
+            return ("%s"):format(tostring(Object)), (" source: %s, what: %s, name: %s (currentline: %s, numparams: %s, nups: %s, is_vararg: %s)"):format(Stringify(Info.source), Info.what, Stringify(Info.name), Info.currentline, Info.numparams, Info.nups, Info.is_vararg)
         else
             return Tostring(Object)
         end
