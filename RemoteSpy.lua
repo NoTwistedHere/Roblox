054361b8e05f11bc2821729b2f9f06076dfd8238
NoTwistedHere <notwistedhere@gmail.com>
2022-05-29 04:18:11 +0100

    Update RemoteSpy.lua

--- RemoteSpy.lua
+++ RemoteSpy.lua
@@ -119,21 +119,23 @@ local function Save(Content)
 end
 
 local function Log(Arguments)
-    for i, v in next, Arguments do
-        if type(v) == "string" then
-            Arguments[i] = Stringify(v)
-        elseif type(v) == "table" then
-            Arguments[i] = PrintTable(v)
+    task.spawn(function()
+        for i, v in next, Arguments do
+            if type(v) == "string" then
+                Arguments[i] = Stringify(v)
+            elseif type(v) == "table" then
+                Arguments[i] = PrintTable(v)
+            end
         end
-    end
 
-    if Arguments.FunctionInfo then
-        return Save(("What: %s\nMethod: %s\nTo Script: %s\nTimestamp: %s\nArguments: %s\nReturn: %s\nInfo: %s\nFunctionInfo: %s\nTraceback: %s\n"):format(Arguments.What, Arguments.Method, Arguments.Script, Arguments.Timestamp, Arguments.Arguments, Arguments.Response, Arguments.Info, Arguments.FunctionInfo, Arguments.Traceback))
-    elseif Arguments.Response then
-        return Save(("What: %s\nMethod: %s\nFrom Script: %s\nTimestamp: %s\nArguments: %s\nReturn: %s\nInfo: %s\nTraceback: %s\n"):format(Arguments.What, Arguments.Method, Arguments.Script, Arguments.Timestamp, Arguments.Arguments, Arguments.Response, Arguments.Info, Arguments.Traceback))
-    end
+        if Arguments.FunctionInfo then
+            return Save(("What: %s\nMethod: %s\nTo Script: %s\nTimestamp: %s\nArguments: %s\nReturn: %s\nInfo: %s\nFunctionInfo: %s\nTraceback: %s\n"):format(Arguments.What, Arguments.Method, Arguments.Script, Arguments.Timestamp, Arguments.Arguments, Arguments.Response, Arguments.Info, Arguments.FunctionInfo, Arguments.Traceback))
+        elseif Arguments.Response then
+            return Save(("What: %s\nMethod: %s\nFrom Script: %s\nTimestamp: %s\nArguments: %s\nReturn: %s\nInfo: %s\nTraceback: %s\n"):format(Arguments.What, Arguments.Method, Arguments.Script, Arguments.Timestamp, Arguments.Arguments, Arguments.Response, Arguments.Info, Arguments.Traceback))
+        end
 
-    Save(("What: %s\nMethod: %s\nFrom Script: %s\nTimestamp: %s\nArguments: %s\nInfo: %s\nTraceback: %s\n"):format(Arguments.What, Arguments.Method, Arguments.Script, Arguments.Timestamp, Arguments.Arguments, Arguments.Info, Arguments.Traceback))
+        Save(("What: %s\nMethod: %s\nFrom Script: %s\nTimestamp: %s\nArguments: %s\nInfo: %s\nTraceback: %s\n"):format(Arguments.What, Arguments.Method, Arguments.Script, Arguments.Timestamp, Arguments.Arguments, Arguments.Info, Arguments.Traceback))
+    end)
 end
 
 local function ArgGuard(self, ...)
